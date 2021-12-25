3d4224128b24d96f73de04f546c779b874f72f0f
madhavrajverma <madhavrajverma175@gmail.com>
2021-12-25 09:59:58 +0530

    Initial Commit

---
+++ JobFinder/JobFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobFinderApp.swift
+//  JobFinder
+//
+//  Created by Madhav Raj Verma on 25/12/21.
+//
+
+import SwiftUI
+
+@main
+struct JobFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
